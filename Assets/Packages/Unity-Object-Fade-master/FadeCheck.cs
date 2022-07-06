6d77f3a010cfbe562da847f99390a62f989dd2ed
Johnny Howe <jonathon.o.howe@gmail.com>
2022-07-07 11:22:56 +1200

    object fade

---
+++ Assets/Packages/Unity-Object-Fade-master/FadeCheck.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeCheck : MonoBehaviour
+{
+    [HideInInspector]
+    public List<GameObject> objectsHit;
+    [HideInInspector]
+    public GameObject parentObjectHit;
+    [Range(0.0f, 1.0f)]
+    public float fadeTo;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = -(transform.position - Camera.main.transform.position).normalized;
+        RaycastHit[] hits = Physics.RaycastAll(transform.position, direction);
+        objectsHit = new List<GameObject>();
+        for (int i = 0; i < hits.Length; i++)
+        {
+            objectsHit.Add(hits[i].transform.gameObject);
+            // GameObject findParent = hit.transform.gameObject;
+            // while (findParent.transform.parent != null)
+            // {
+            //     findParent = findParent.transform.parent.gameObject;
+            // }
+            // parentObjectHit = findParent;
+
+            // Debug.DrawLine(Camera.main.transform.position, hit.transform.position, Color.green);
+        }
+        // Debug.DrawRay(Camera.main.transform.position, direction * 50);
+    }
+}
---
+++ Assets/Packages/Unity-Object-Fade-master/ObjectFade.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectFade : MonoBehaviour
+{
+    public float fadeSpeed = 10f;
+    public float fadeTo = 1f;
+    public float fadeFrom = 0.5f;
+
+    FadeCheck fadeCheck;
+    Renderer objectRenderer;
+
+    Material uniqueMaterial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fadeCheck = GameObject.Find("Player").GetComponent<FadeCheck>();
+
+        objectRenderer = GetComponent<Renderer>();
+        uniqueMaterial = TransparentCopy(objectRenderer.material);
+        objectRenderer.material = uniqueMaterial;
+    }
+
+    Material TransparentCopy(Material toCopy) {
+        Material material = Instantiate(toCopy);
+        material.SetOverrideTag("RenderType", "Transparent");
+        material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+        material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+        material.SetInt("_ZWrite", 0);
+        material.DisableKeyword("_ALPHATEST_ON");
+        material.EnableKeyword("_ALPHABLEND_ON");
+        material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+        material.renderQueue = (int)UnityEngine.Rendering.RenderQueue.Transparent;
+        return material;
+    }
+
+    void Update()
+    {
+        float targetAlpha = ShouldFade()? fadeFrom : fadeTo;
+        float nextAlpha = Mathf.Lerp(objectRenderer.material.color.a, targetAlpha, Mathf.Min(1, Time.deltaTime * fadeSpeed));
+        SetAlpha(objectRenderer.material, nextAlpha);
+    }
+
+    bool ShouldFade() {
+        return fadeCheck.objectsHit.Contains(gameObject);
+    }
+
+    void SetAlpha(Material mat, float alpha) {
+        Color c = mat.color;
+        c.a = alpha;
+        mat.color = c;
+    }
+}
