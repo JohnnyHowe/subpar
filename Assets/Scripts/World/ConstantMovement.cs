670e367ab6ba15c315888566a990f893d4c3e400
Johnny Howe <jonathon.o.howe@gmail.com>
2022-10-26 20:04:15 +1300

    (feature) ConstantMovement component works with rotation

--- Assets/Scripts/World/ConstantMovement.cs
+++ Assets/Scripts/World/ConstantMovement.cs
@@ -6,27 +6,84 @@ public class ConstantMovement : MonoBehaviour
 {
     [SerializeField] private Transform positionsContainer;
     [SerializeField] private Rigidbody movingObject;
-    [SerializeField] private float speed = 1;
+    [Header("Movement Configuration")]
+    [SerializeField] private bool constantVelocity = false;
+    [SerializeField] private float roundTripTime = 1;
+    [SerializeField] private float smoothSpeed = 0f;
 
     private float t;
+    private List<float> consistentSpeedMultipliers;
 
     void Start()
     {
-        t = 0; 
+        t = 0;
+        CalculatePath();
+    }
+
+    void CalculatePath()
+    {
+        CalculatePathDistances();
     }
 
     void FixedUpdate()
     {
-        t += Time.fixedDeltaTime * speed;
-        t = t % positionsContainer.childCount;
+        if (roundTripTime == 0) return;
 
+        if (Debug.isDebugBuild)
+        {
+            CalculatePath();
+        }
+
+        t = t % positionsContainer.childCount;
         int p1t = Mathf.FloorToInt(t);
         int p2t = (p1t + 1) % positionsContainer.childCount;
+
+        float speed;
+        if (constantVelocity)
+        {
+            speed = consistentSpeedMultipliers[p1t] / roundTripTime;
+        }
+        else
+        {
+            speed = positionsContainer.childCount / roundTripTime;
+        }
+        float transformT = smoothSpeed == 0? 1: Time.deltaTime * smoothSpeed * positionsContainer.childCount;
 
         Vector3 p1 = positionsContainer.GetChild(p1t).position;
         Vector3 p2 = positionsContainer.GetChild(p2t).position;
 
-        Vector3 pos = Vector3.Lerp(p1, p2, t % 1);
-        movingObject.MovePosition(pos);
+        Vector3 targetPos = Vector3.Lerp(p1, p2, t % 1);
+        Vector3 nextPos = Vector3.Lerp(movingObject.transform.position, targetPos, transformT);
+        movingObject.MovePosition(nextPos);
+
+        Quaternion r1 = positionsContainer.GetChild(p1t).rotation;
+        Quaternion r2 = positionsContainer.GetChild(p2t).rotation;
+
+        Quaternion targetRotation = Quaternion.Lerp(r1, r2, t % 1);
+        Quaternion nextRot = Quaternion.Lerp(movingObject.transform.rotation, targetRotation, transformT);
+        movingObject.MoveRotation(nextRot);
+
+        t += Time.fixedDeltaTime * speed;
+    }
+
+    private void CalculatePathDistances()
+    {
+        float sum = 0;
+        List<float> distances = new List<float>();
+        for (int i = 0; i < positionsContainer.childCount; i++)
+        {
+            float currentDistance = (
+                positionsContainer.GetChild(i % positionsContainer.childCount).position - 
+                positionsContainer.GetChild((i + 1) % positionsContainer.childCount).position
+                ).magnitude;
+
+            distances.Add(currentDistance);
+            sum += currentDistance;
+        }
+        List<float> speedMultipliers = new List<float>();
+        for (int i = 0; i < positionsContainer.childCount; i++) {
+            speedMultipliers.Add(sum / distances[i]);
+        }
+        consistentSpeedMultipliers = speedMultipliers;
     }
 }
