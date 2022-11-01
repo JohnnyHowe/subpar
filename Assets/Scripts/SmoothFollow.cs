236c5756dc8e7558508341de16057e38d878213f
Johnny Howe <jonathon.o.howe@gmail.com>
2022-11-01 20:25:44 +1300

    Game setup is its own prefab

--- Assets/Scripts/SmoothFollow.cs
+++ Assets/Scripts/SmoothFollow.cs
@@ -3,14 +3,21 @@ using System.Collections;
 
 public class SmoothFollow : MonoBehaviour {
     
-	public Transform target;
+	// public Transform target;
+	public string targetTag;
     public Vector3 offset = new Vector3(10, 10, 10);
     public float speed = 1f;
 
+	private CachedMember<Transform> target;
+
+	void Start() {
+		target = new CachedMember<Transform>(() => GameObject.FindGameObjectWithTag(targetTag).transform);
+	}
+
 	void LateUpdate () {
 		// Early out if we don't have a target
-		if (!target) return;
+		if (target.Equals(null)) return;
 
-        transform.position = Vector3.Lerp(transform.position, target.position + offset, Time.deltaTime * speed);
+        transform.position = Vector3.Lerp(transform.position, target.Value.position + offset, Time.deltaTime * speed);
 	}
 }
